8221b0c91ba57dbc5e6eafeaee26d50d5044cf33
Isaac Ashwin <ashiswin@gmail.com>
2018-11-09 16:36:15 +0800

    Fixed typo

--- app/config.py
+++ app/config.py
@@ -11,5 +11,5 @@ class Config(object):
 class DevConfig(object):
     DEBUG = True
     RELOAD = True
-    SQLALCHEMY_DATABASE_URI = "mysql://asimov:asimov@localhost/asimov?host=localhost?port=3306"
+    SQLALCHEMY_DATABASE_URI = "mysql://asimov:asimov@localhost/asimov"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
